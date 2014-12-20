3fde573a276f648aaf95f5ef8d27a8b5a1e5cab5
SATSU <sawahidoshi@hotmail.com.tw>
2014-12-20 15:12:37 +0800

    鏡頭 跳躍

--- Assets/New Folder/player.js
+++ Assets/New Folder/player.js
@@ -1,14 +1,25 @@
 ﻿#pragma strict
 
+var isJumping : boolean = false;
 var speed : float = 5.0f;
-
 var jumpForce : float = 300.0f;
 
 
 function Update () {
 	rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
 	
-	if(Input.GetButtonDown("Jump")){
+	if(Input.GetButtonDown("Jump") && isJumping==false){
 		rigidbody2D.AddForce(new Vector2(0, jumpForce));
 	}
 }
+function OnTriggerEnter2D (other : Collider2D ){
+	if (other.tag == "ground")
+		isJumping = false;
+}
+
+function OnTriggerExit2D (other : Collider2D ){
+	if (other.tag == "ground")
+		isJumping = true;
+
+}
+
